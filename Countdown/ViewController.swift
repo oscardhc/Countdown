035a26c7f2391c077683ab15a4f2cda9afa8abc0
oscardhc <oscardhc@sjtu.edu.cn>
2018-12-28 14:28:18 +0800

    Initial Commit

---
+++ Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Haichen Dong on 2018/12/28.
+//  Copyright © 2018 Haichen Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

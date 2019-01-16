56d0fa9e40fe1b15e40ccd021307c615c0d909b2
oscardhc <oscardhc@sjtu.edu.cn>
2019-01-16 21:55:40 +0800

    edit

--- Countdown/AppDelegate.swift
+++ Countdown/AppDelegate.swift
@@ -13,8 +13,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.main.bounds)
